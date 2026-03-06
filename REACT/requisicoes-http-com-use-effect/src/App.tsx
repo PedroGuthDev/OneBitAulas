bb82faeec739fe9ed79123733ee999a9a0717959
Pedro Henrique Alberton Guth <pedrohaguth943@gmail.com>
2026-03-06 09:11:16 -0300

    Aprendido que o useEffect deve ser usado somente em situações necessárias, e nunca para eventos que vão gerar um loop de renderização.

--- REACT/requisicoes-http-com-use-effect/src/App.tsx
+++ REACT/requisicoes-http-com-use-effect/src/App.tsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-
+import { useState } from "react";
 async function fetchPokemon() {
   const response = await fetch("https://pokeapi.co/api/v2/pokemon");
   const data: { results: { name: string; url: string }[] } =
@@ -7,29 +6,114 @@ async function fetchPokemon() {
   return data.results;
 }
 
+async function fetchPokemonData(url: string) {
+  const response = await fetch(url);
+  const data: Pokemon = await response.json();
+  return data;
+}
+
+type Pokemon = {
+  name: string;
+  sprites: {
+    front_default: string;
+  };
+  types: {
+    type: {
+      name: string;
+    };
+  }[];
+  height: number;
+  weight: number;
+  stats: {
+    base_stat: number;
+    stat: {
+      name: string;
+    };
+  }[];
+  abilities: {
+    ability: {
+      name: string;
+    };
+    is_hidden: boolean;
+  }[];
+};
+
 export default function App() {
   const [pokemon, setPokemon] = useState<{ name: string; url: string }[]>([]);
-  useEffect(() => {
+  const [pokemonShown, setPokemonShown] = useState<Pokemon | null>(null);
+  // Ao invés de um useEffect, usamos um if para buscar os dados
+  if (pokemon.length === 0) {
     fetchPokemon().then((results) => setPokemon(results));
-  }, []);
+  }
 
   return (
-    <div>
-      <h1>App</h1>
-      <ul>
-        {pokemon.map((pokemon) => (
-          <div key={pokemon.name}>
-            <li>{pokemon.name}</li>
-            <button
-              onClick={() =>
-                window.open(pokemon.url, "_blank.noopener.noreferrer")
-              }
-            >
-              Ver detalhes
-            </button>
+    <div id="app" style={{ display: "flex", gap: "1rem" }}>
+      <div>
+        <h1>Pokemons</h1>
+        <ul
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+          }}
+        >
+          {pokemon.map((pokemon) => (
+            <li key={pokemon.name} style={{ display: "flex", gap: "1rem" }}>
+              <span style={{ textTransform: "capitalize", padding: "0.5rem" }}>
+                {pokemon.name}
+              </span>
+              <button
+                onClick={async () => {
+                  setPokemonShown(await fetchPokemonData(pokemon.url));
+                }}
+              >
+                Ver detalhes
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {pokemonShown && (
+        <div>
+          <h2>{pokemonShown.name}</h2>
+          <img src={pokemonShown.sprites.front_default} alt="" />
+          <div className="stat">
+            <b>Tipo: </b>
+            {pokemonShown.types.map(({ type }) => (
+              <span key={type.name}>{type.name} </span>
+            ))}
           </div>
-        ))}
-      </ul>
+          <div className="stat">
+            <b>Altura: </b>
+            {pokemonShown.height / 10} m
+          </div>
+          <div className="stat">
+            <b>Peso: </b>
+            {pokemonShown.weight / 10} Kg
+          </div>
+          <div className="stat">
+            <b>Atributos</b>
+            <ul>
+              {pokemonShown.stats.map(({ base_stat, stat }) => (
+                <li key={stat.name}>
+                  {stat.name}: {base_stat}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="stat">
+            <b>Habilidades</b>
+            <ul>
+              {pokemonShown.abilities.map(({ ability, is_hidden }) => (
+                <li key={ability.name}>
+                  {ability.name}
+                  {is_hidden && " (secreta)"}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
